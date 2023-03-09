0682fc98f233c6f295ec4fb0d212684d3fce2f39
Holly Zaharchuk <15241450+hollzzar@users.noreply.github.com>
2023-03-08 20:28:18 -0700

    update affiliations

--- compile_poster.R
+++ compile_poster.R
@@ -3,3 +3,5 @@ rmarkdown::render("HSP_2023.Rmd")
 path <- "file://localhost/Users/hollyzaharchuk/Mirror/Project_CS/presenting/"
 file <- paste0(path, "HSP_2023.html")
 pagedown::chrome_print(file)
+
+
